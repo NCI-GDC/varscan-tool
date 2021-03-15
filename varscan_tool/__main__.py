098c73474a0e6a13b6cc7be96f301b30d787c395
Shenglai <lyellooo@gmail.com>
2021-03-15 21:13:28 +0000

    BINF-481: fix * fix entry * fix docker source

--- varscan_tool/__main__.py
+++ varscan_tool/__main__.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python3
+import sys
 
 from varscan_tool import multi_varscan
 
 if __name__ == "__main__":
-    multi_varscan.main()
+    # CLI Entrypoint.
+    retcode = 0
+
+    try:
+        retcode = multi_varscan.main()
+
+    except Exception as e:
+        retcode = 1
+
+    sys.exit(retcode)
 
 # __END__
